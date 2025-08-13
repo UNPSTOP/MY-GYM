46dd51337aba30b948e0ababc77d7c69dff14c0e
Mainuddin <mainuddinmd464@gmail.com>
2025-08-13 16:11:13 +0530

    update payments in models

--- Backend/backend/gymapi/models.py
+++ Backend/backend/gymapi/models.py
@@ -6,7 +6,7 @@ class Member(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     password = models.CharField(max_length=128)
-    payment_status = models.CharField(max_length=50, null=True, blank=True)
+    payment_status = models.BooleanField(default=False)
     payment_id = models.CharField(max_length=100, null=True, blank=True)
     membership_type = models.CharField(max_length=50)
     joined_on = models.DateField(auto_now_add = True)
